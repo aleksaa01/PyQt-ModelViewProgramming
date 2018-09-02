ef6ad14bd30f37193ae906065cfccbc3a058b881
aleksaa01 <some12curious@gmail.com>
2018-09-02 16:04:28 +0200

    Adding methods for insertion and deletion of items in the model.

--- custom_models.py
+++ custom_models.py
@@ -1,22 +1,24 @@
 from utils import AppWindow
 
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QAbstractListModel, Qt
+from PyQt5.QtCore import QAbstractListModel, Qt, QModelIndex
 from PyQt5.QtGui import QPixmap, QIcon, QColor
 
 import sys
 
 
 class LanguagesListModel(QAbstractListModel):
+    """
     # Custom model for ListView
     # required methods: rowCount, data
     # requied methods for making model editable: flags, setData
+    """
 
     def __init__(self, languages=None, parent=None):
         super().__init__(parent)
         self._languages = languages if languages else []
 
-    def rowCount(self, parent):
+    def rowCount(self, parent=QModelIndex()):
         return len(self._languages)
 
     def data(self, index, role):
@@ -51,6 +53,30 @@ class LanguagesListModel(QAbstractListModel):
             return True
         return False
 
+    def insertRows(self, position, rows, parent=QModelIndex()):
+        # Methods: beginInsertRows() and endInsertRows()
+        # They emit a signal that notifies the views,
+        # so that views can respond corectly.
+        self.beginInsertRows(parent, position, position + rows - 1)
+
+        for i in range(rows):
+            self._languages.insert(position, 'Generic language ' + str(rows - i))
+
+        self.endInsertRows()
+        return True
+
+    def removeRows(self, position, rows, parent=QModelIndex()):
+        # Methods: beginRemoveRows() and endRemoveRows()
+        # They emit a signal that notifies the views,
+        # so that views can respond corectly.
+        self.beginRemoveRows(parent, position, position + rows - 1)
+
+        for i in range(rows):
+            del self._languages[position]
+
+        self.endRemoveRows()
+        return True
+
     def addData(self, new_data):
         self._languages = new_data
 
@@ -67,6 +93,7 @@ if __name__ == '__main__':
 
     app_window = AppWindow(model=language_model)
     app_window.add_test_widgets()
+    app_window.insertModelRows(4, 5)
     app_window.show()
-
+    app_window.removeModelRows(4, 4)
     sys.exit(app.exec_())
