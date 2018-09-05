62627a7f02e577bf9dbb933e9238c9273ac41782
aleksaa01 <some12curious@gmail.com>
2018-09-05 11:49:58 +0200

    Simplifying basic example by creating new very basic class, instead of using more complicated class from utils, which is not needed for this example.

--- basic_example.py
+++ basic_example.py
@@ -1,12 +1,45 @@
-from utils import AppWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QApplication, \
+    QListView, QComboBox
+from PyQt5.QtCore import QStringListModel
 
-from PyQt5 import QtWidgets, QtCore
 import sys
 
 
+class AppWindow(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.setFixedSize(640, 480)
+
+        data = ['one', 'two', 'three', 'four', 'five']
+        model = QStringListModel(data)
+
+        layout = QVBoxLayout()
+
+        listview = QListView()
+        combobox = QComboBox()
+        listview2 = QListView()
+        combobox2 = QComboBox()
+
+        listview.setModel(model)
+        combobox.setModel(model)
+        listview2.setModel(model)
+        combobox2.setModel(model)
+
+        layout.addWidget(listview)
+        layout.addWidget(combobox)
+        layout.addWidget(listview2)
+        layout.addWidget(combobox2)
+
+        central_widget.setLayout(layout)
+
+
+
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     app_window = AppWindow()
-    app_window.add_test_widgets()
     app_window.show()
     sys.exit(app.exec_())
