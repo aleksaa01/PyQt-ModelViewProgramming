9ea8bd3ba518fc42ba38ded488e330578a667782
aleksaa01 <some12curious@gmail.com>
2018-09-12 13:05:10 +0200

    Adding optimized version of methods for inserting and removing rows and columns.

--- custom_table_model.py
+++ custom_table_model.py
@@ -1,7 +1,7 @@
 from utils import AppWindow
 
 from PyQt5.QtWidgets import QApplication, QTableWidget, QTableView
-from PyQt5.QtCore import Qt, QAbstractTableModel
+from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex
 from PyQt5.QtGui import QPixmap, QIcon, QColor
 
 import sys
@@ -62,9 +62,58 @@ class LanguagesTableModel(QAbstractTableModel):
     def headerData(self, section, orientation, role):
         if role == Qt.DisplayRole:
             if orientation == Qt.Horizontal:
-                return self._headers[section]
+                # Check the length in case of insertColumns !
+                if section < len(self._headers):
+                    return self._headers[section]
+                else:
+                    return 'Others'
             else:
                 return 'Popularity rank {}'.format(section)
+
+    def insertRows(self, position, rows, parent=QModelIndex()):
+        self.beginInsertRows(parent, position, position + rows - 1)
+
+        default_row = ['Popular language' for _ in range(self.columnCount(parent))]
+        for i in range(rows):
+            self._languages.insert(position, default_row)
+
+        self.endInsertRows()
+        return True
+
+    def removeRows(self, position, rows, parent=QModelIndex()):
+        self.beginRemoveRows(parent, position, position + rows - 1)
+
+        for i in range(rows):
+            del self._languages[position]
+
+        self.endRemoveRows()
+        return True
+
+    def insertColumns(self, position, columns, parent=QModelIndex()):
+        # When you insert new columns you will have to expand the headers
+        self.beginInsertColumns(parent, position, position + columns - 1)
+
+        rows = len(self._languages)
+
+        # faster way then repeatedly inserting at "position"
+        new_values = ['Popular language' for i in range(columns)]
+        for i in range(rows):
+            self._languages[i][position:position] = new_values
+
+        self.endInsertColumns()
+        return True
+
+    def removeColumns(self, position, columns, parent=QModelIndex()):
+        self.beginRemoveColumns(parent, position, position + columns - 1)
+
+        num_rows = len(self._languages)
+
+        # faster method
+        for i in range(num_rows):
+            del self._languages[i][position:position + columns]
+
+        self.endRemoveColumns()
+        return True
 
     def addData(self, new_data):
         if type(new_data) != list:
@@ -97,6 +146,11 @@ if __name__ == '__main__':
 
     table_widget = QTableView()
     app_window.addWidget(table_widget, set_model=True)
+
+    app_window.insertModelRows(4, 3)
+    app_window.removeModelRows(4, 2)
+    app_window.insertModelColumns(0, 5)
+    app_window.removeModelColumns(1, 4)
 
     app_window.show()
 
